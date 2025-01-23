cda39b296610a3ea0c5fc9e81dabe50c68a800a5
MhmoudMoawad <moawadmhmoud@gmail.com>
2025-01-22 20:08:08 -0500

    problem complete

---
+++ day13/practice1.py
@@ -0,0 +1,16 @@
+#This function has a bug 
+
+#def my_function():
+#    for i in range(1,20)
+#    if i == 20:
+#        print("you got it")
+#my_function()
+
+#This function is teh same code but with the bug removed
+
+def my_function():
+    for i in range(1,21):
+        if i == 20:
+            print("you got it")
+            
+my_function()
